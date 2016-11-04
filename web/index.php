3df0cb281a158d8f41970f9a09bd2d3ae32d24bc
Jan Decavele <jandecavele@gmail.com>
2016-11-04 15:04:06 +0100

    removed filesystem cache (gae doesn't support it)

--- web/index.php
+++ web/index.php
@@ -1,7 +1,6 @@
 <?php
 
 use Silex\Application;
-use Silex\Provider\HttpCacheServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Symfony\Component\HttpFoundation\Response;
 use TwigWrapperProvider\Processors\CriticalCssProcessor;
@@ -18,27 +17,22 @@ $app->register(new TwigServiceProvider(), [
     'twig.path' => __DIR__ . '/views',
     'twig.options' => [
         'debug' => $debug,
-        'cache' => __DIR__ . '/../cache/twig_cache/',
     ]
 ]);
 $app->register(new TwigWrapperProvider('twig', [new CriticalCssProcessor()]));
 
-$app->register(new HttpCacheServiceProvider(), array(
-    'http_cache.cache_dir' => __DIR__ . '/../cache/http_cache/',
-));
-
 $app['debug'] = $debug;
 
 $app->get('/', function () use ($app, $cacheTtl) {
-    return Response::create($app['twig']->render('index.twig'))->setTtl($cacheTtl);
+    return Response::create($app['twig']->render('index.twig'));
 });
 
 $app->get('/critical', function () use ($app,$cacheTtl) {
-    return Response::create($app['twig']->render('critical.twig'))->setTtl($cacheTtl);
+    return Response::create($app['twig']->render('critical.twig'));
 });
 
 $app->get('/reference', function () use ($app,$cacheTtl) {
-    return Response::create($app['twig']->render('reference.twig'))->setTtl($cacheTtl);
+    return Response::create($app['twig']->render('reference.twig'));
 });
 
-$app['http_cache']->run();
+$app->run();
