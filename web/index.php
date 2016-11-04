10a3bd07d658bfd26708c4ab4329bb6c9f63766c
Jan Decavele <jandecavele@gmail.com>
2016-11-04 13:10:41 +0100

    basic silex scaffold

--- web/index.php
+++ web/index.php
@@ -1,1 +1,22 @@
-<h1>Hello world!</h1>
+<?php
+
+use Silex\Application;
+
+$loader = include __DIR__ . '../vendor/autoload.php';
+
+$app = new Application();
+
+
+$app->get('/', function () use ($app) {
+    return 'index';
+});
+
+$app->get('/critical', function () use ($app) {
+    return 'processed';
+});
+
+$app->get('/reference', function () use ($app) {
+    return 'reference';
+});
+
+$app->run();
